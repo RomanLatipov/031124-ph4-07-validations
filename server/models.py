9cfb663be7ceaedda9eaa64ad9811a44bb7d7764
romanlatipov <romanlatipov@gmail.com>
2024-05-16 14:30:55 -0400

    complete validations

--- server/models.py
+++ server/models.py
@@ -19,10 +19,27 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    username = db.Column(db.String)
+    username = db.Column(db.String, nullable=False)
     email = db.Column(db.String)
     address = db.Column(db.String)
     phone_number = db.Column(db.String)
     age = db.Column(db.Integer)
     vip = db.Column(db.Boolean)
     year_joined = db.Column(db.Integer)
+
+    @validates('username')
+    def validate_username(self, key, value):
+        user = User.query.where(User.username == value).first()
+        if user:
+            raise ValueError("User already exist")
+        if len(value.replace(" ", "")) >= 4:
+            return value.strip()
+        else:
+            raise ValueError("Username must be more than 4 characters")
+        
+    @validates('age')
+    def validate_age(self, key, value):
+        if value >= 13:
+            return value
+        else:
+            raise ValueError("User must be older than 13")
